96713edf9700d1bb64814e7592529f6faa028fa8
jobsonjai <jobsonjai123@gmail.com>
2024-07-05 03:12:04 -0500

    Fix: Just new line added in the .txt and add.py file

--- add.py
+++ add.py
@@ -3,5 +3,6 @@ def addition(a, b):
 
 num1 = 5
 num2 = 3
-result = addition(num1, num2)
-print(f"{num1} + {num2} = {result}")
+num3 = 6
+result = addition(num1, num2, num3)
+print(f"{num1} + {num2} + {num3} = {result}")
